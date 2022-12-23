2e6c6ce9514a52c6fd81a1d08534407c7b40457f
Joel Hacke <joel.hacke@wwt.com>
2022-12-23 16:39:03 -0600

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, tempora
+      accusamus sint doloribus modi recusandae ducimus nemo dolore cum, fuga
+      pariatur debitis! Aut recusandae dolor magnam! Excepturi est quod
+      repellat.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+P {
+  margin: 20px 0;
+}
+</style>
